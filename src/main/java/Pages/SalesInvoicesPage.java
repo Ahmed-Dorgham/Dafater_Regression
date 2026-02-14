e809233bd216ea175b6d6c08045d2617f70b5635
Ahmed_Dorgham_MINE <dorgham@gmail.com>
2026-02-14 15:01:50 +0200

    test trigger development 2

--- src/main/java/Pages/SalesInvoicesPage.java
+++ src/main/java/Pages/SalesInvoicesPage.java
@@ -19,7 +19,8 @@ public class SalesInvoicesPage extends MainPage {
             " | //input[contains(@data-fieldname,'update_stock')]");
     private By validationMsg = By.className("msgprint");
     private By editIcon = By.className("icon-xs");
-    // فثسف
+    // test
+
     private By newSalesInvoiceTitle = By.xpath("//*[contains(@title,'فاتورة المبيعات جديد')]");
     private By customerFieldSalesInvoice = By.xpath("(//*[contains(@id,'customer')])[4]");
     private By itemCodeField = By.xpath("(//*[contains(@data-fieldname,'item_code')])[2]");
