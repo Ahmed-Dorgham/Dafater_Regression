e6929bf764001401ec3e00b0a44cf2cad6b76c61
AhmedDorgham <ahmeddorgham5000@gmail.com>
2026-03-24 00:02:16 +0200

    //test PC 1

--- src/test/java/TestCases/ComparingBanksTest.java
+++ src/test/java/TestCases/ComparingBanksTest.java
@@ -13,7 +13,7 @@ public class ComparingBanksTest extends BaseTest {
     int randomNumber;
     String itemCode;
     SoftAssert softAssert;
-
+//test PC 1
     LoginPage loginPageObj;
     HomePage homePageObj;
     JournalEntrytListPage journalEntryListPageObj;
